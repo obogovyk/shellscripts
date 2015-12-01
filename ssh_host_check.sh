e94a67e86f2e9c77a97ad827fd56b833584810fc
Oleksandr Bogovyk <obogovyk@gmail.com>
2015-12-01 13:45:20 +0200

    Create ssh_host_check.sh

---
+++ ssh_host_check.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+username="{username}"
+hosts=(
+host1.example.com
+host2.example.com
+host3.example.com
+)
+
+for i in ${hosts[*]}; do
+    ping -c 1 $i &> /dev/null
+    if [ $? -eq 0 ]; then
+        livehosts+=($i)
+        echo "$i: Ping success."
+        x=$((x+1))
+    else
+        diedhosts+=($i)
+    fi
+done
+
+echo ""
+echo "Found: $x live hosts."
+
+for i in ${livehosts[*]}; do 
+    z=$(ssh $username@$i 'hostname -f')
+
+    if [ "$i" == "$z" ]; then
+        echo -e "\033[1mHost $i equal hostname $z!\033[0m"
+    else
+        echo "Host $i not equal hostname $z!" > /opt/hosts.notequal
+        echo "Host $i not equal hostname $z!"
+    fi
+done
+
+for i in ${diedhosts[*]}; do 
+    echo "$i: died (no ping)." > /opt/hosts.noping
+done
