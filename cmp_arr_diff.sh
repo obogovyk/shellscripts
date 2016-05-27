7eee4e8e3e5e12fb38e47d75cb6acd750cdb1a33
Oleksandr Bogovyk <obogovyk@gmail.com>
2016-05-27 17:05:31 +0300

    Update cmp_arr_diff.sh

--- cmp_arr_diff.sh
+++ cmp_arr_diff.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Script: jslint.sh
+# Author: Bogovyk Oleksanr <obogovyk@gmail.com>
 
 nums1=( 3 5 8 4 9 2 )
 nums2=( 3 1 7 8 9 10 )
@@ -16,6 +18,6 @@ for x in ${nums1[@]}; do
     [[ -n $skip ]] || noteq+=("$x")
 done
 
-for i in ${noteq[*]}; do 
+for i in ${noteq[@]}; do 
     echo $i
 done
