7442e83fec56f1b8410c79dde587fcd0101deb54
Oleksandr Bogovyk <obogovyk@gmail.com>
2016-07-11 12:50:02 +0300

    Create minifier.sh

---
+++ minifier.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+MINIFY=$(whereis minify|awk {'print $2'})
+TYPES=( "*.css" ) # "*.js"
+FILES=()
+
+if [ ! -z "$MINIFY" ]; then
+    for a in ${TYPES[@]}; do
+        for i in $(find "`pwd`/frontend" -type f -name $a ); do
+            if [[ $i != *".min."* ]]; then
+                FILES+=($i)
+            fi
+        done
+    done
+else
+    echo -e "[ERROR]: Minifier not found!\n[INSTALL]: sudo npm install minifier -g"
+    exit 1
+fi
+
+for f in ${FILES[@]}; do
+    $MINIFY $f
+done
