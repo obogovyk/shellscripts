52edb24e5dd3ecd81befcb178c79bd9bfda3ca58
Oleksandr Bogovyk <obogovyk@gmail.com>
2016-07-18 18:45:23 +0300

    Update mkarch-logs.sh

--- mkarch-logs.sh
+++ mkarch-logs.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Description: runtime logs archive
-
 LOGS=($(ls|grep '[0-9]'|sort -n))
-LATEST=$(ls|grep '[0-9]'|sort -n|tail -n 1)
+LATEST_N=$(ls | egrep -o '[0-9]*'|sort -n|tail -n 1)
 
-if [ ${#LOGS[@]} -gt 2 ]; then
+if [ ${#LOGS[@]} -gt 1 ]; then
     for i in ${LOGS[@]}; do
-        if [ $i != $LATEST ] && [ $i != "runtime1.log" ]; then
+        if [ $i != "runtime$LATEST_N.log" ]; then
             tar cvzf "$i.tar.gz" $i
             mv "$i.tar.gz" "../../logs-app/"
             rm -f $i
