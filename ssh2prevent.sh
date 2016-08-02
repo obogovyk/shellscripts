91e601bdf03f0ac357c025d9891daaefbdb081be
Oleksandr Bogovyk <obogovyk@gmail.com>
2016-08-02 14:17:57 +0300

    Update ssh2prevent.sh

--- ssh2prevent.sh
+++ ssh2prevent.sh
@@ -5,15 +5,17 @@
 
 export LC_ALL=en_US.utf8
 
-CHAIN="SSH2PREVENT"
-RULE_NUM=1
+CHAIN="CR-SSH2PREVENT"
 INTERFACE="eth0"
 SSH_PORT=22
-FILTER="([0-9]{1,3}[\.]){3}[0-9]{1,3}"
+D_FILTER="([0-9]{1,3}[\.]){3}[0-9]{1,3}"
+W_FILTER="[I|i]nvalid\|[C|c]losed\|[D|d]isconnect"
 COUNTER=5
+RULE_NUM=1
+IPTABLES=$(whereis iptables|awk {'print $2'})
 SSH_LOG="/var/log/secure"
-IP_IGNORE_LIST=( "127.0.0.1" "133.33.22.11" )
-IP_BLACK_LIST=( $(cat $SSH_LOG | grep -E -o "$FILTER" | sort | uniq) )
+IP_IGNORE_LIST=( "127.0.0.1" "11.22.33.44" )
+IP_BLACK_LIST=( $(cat $SSH_LOG | grep $W_FILTER | grep -E -o $D_FILTER | sort | uniq) )
 FILTERED_LIST=()
 
 for x in ${IP_BLACK_LIST[@]}; do
@@ -28,31 +30,32 @@ for x in ${IP_BLACK_LIST[@]}; do
 done
 
 chain_exists() {
-    iptables -nL $CHAIN &> /dev/null
+    $IPTABLES -nL $CHAIN &> /dev/null
 }
 
-#chain_clear() {
-#    iptables -F $CHAIN
-#}
+chain_clear() {
+    $IPTABLES -F $CHAIN
+}
 
 iptables_save(){
-    service iptables save
-    service iptables restart
+    /sbin/service iptables save
 }
 
 chain_exists
 if [ $? != 0 ]; then
-    iptables -N $CHAIN
-    iptables -A $CHAIN -j RETURN
+    $IPTABLES -N $CHAIN
 fi
 
-#chain_clear
+chain_clear
 for i in ${FILTERED_LIST[@]}; do
     if [ $(grep -c $i $SSH_LOG) -ge $COUNTER ]; then
-        if [ $(iptables -nL $CHAIN | grep $i | wc -l) -eq 0 ]; then
-            iptables -I $CHAIN $RULE_NUM -i $INTERFACE -s $i -p tcp -m tcp --dport $SSH_PORT -j REJECT --reject-with tcp-reset
-        fi
+        $IPTABLES -A $CHAIN -i $INTERFACE -s $i -p tcp -m tcp --dport $SSH_PORT -j REJECT --reject-with icmp-port-unreachable
     fi
 done
+$IPTABLES -A $CHAIN -j RETURN
+
+if [ $($IPTABLES -nL INPUT | grep $CHAIN | wc -l) -eq 0 ]; then
+    $IPTABLES -I INPUT $RULE_NUM -i $INTERFACE -p tcp -m multiport --dports $SSH_PORT -j $CHAIN
+fi
 
 iptables_save
