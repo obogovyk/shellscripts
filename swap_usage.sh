39dd16cee45b16475f3640c8c29bb1c7583fd18e
Oleksandr Bogovyk <obogovyk@gmail.com>
2016-10-14 17:12:24 +0300

    Update swap_usage.sh

--- swap_usage.sh
+++ swap_usage.sh
@@ -14,10 +14,10 @@ for DIR in $(find /proc/ -maxdepth 1 -type d | egrep "^/proc/[0-9]") ; do
    PID=$(echo $DIR | cut -d / -f 3)
    PROG=$(ps -p $PID -o comm --no-headers)
    for SWAP in $(grep Swap $DIR/smaps 2>/dev/null | awk '{ print $2 }'); do
-      let SUM=$SUM+$SWAP
+      SUM=(( $SUM+$SWAP ))
    done
  echo "PID=$PID - Swap used: ${SUM}K - ($PROG)"
- let OVERALL=$OVERALL+$SUM
+ OVERALL=(( $OVERALL+$SUM ))
  SUM=0
 done
 
