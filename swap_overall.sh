f016bdfd83b6ebecec7eefea2138b2c2056a5c03
Oleksandr Bogovyk <obogovyk@gmail.com>
2016-10-14 17:07:08 +0300

    Update and rename swap_stat.sh to swap_overall.sh

---
+++ swap_overall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SUM=0
+OVERALL=0
+
+if [ "$UID" != "0" ]; then
+   echo "This script must be run as root or with sudo." 1>&2
+   exit 1
+fi
+
+for DIR in $(find /proc/ -maxdepth 1 -type d | egrep "^/proc/[0-9]") ; do
+   PID=$(echo $DIR | cut -d / -f 3)
+   PROG=$(ps -p $PID -o comm --no-headers)
+   for SWAP in $(grep Swap $DIR/smaps 2>/dev/null | awk '{ print $2 }'); do
+      let SUM=$SUM+$SWAP
+   done
+ echo "PID=$PID - Swap used: ${SUM}K - ($PROG)"
+ let OVERALL=$OVERALL+$SUM
+ SUM=0
+done
+
+echo "Overall swap used: $OVERALL"
