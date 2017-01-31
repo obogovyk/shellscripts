6ffb0567b382925e3ae0278803ac95d950b8850f
Oleksandr Bogovyk <obogovyk@gmail.com>
2017-01-31 17:19:08 +0200

    Update check_red5.sh

--- check_red5.sh
+++ check_red5.sh
@@ -9,10 +9,10 @@ COUNT=0
 PROBES=4
 
 while [ "${CHK}" != "200" ]; do
-    (( COUNT++ ))
     echo "Check starting Server, (${COUNT}) probe..."
     sleep 30
     CHK=`${CURL} --silent -I ${APP}| head -1| awk {'print $2'}`
+    (( COUNT++ ))
 
     if [ ${COUNT} -ge ${PROBES} ]; then
         echo "[ERROR]: Server failed to start after (${COUNT}) probes."
