b9534de3ae8dee04c4dc0a84fba59cb5adcfce8a
Oleksandr Bogovyk <obogovyk@gmail.com>
2017-01-31 17:08:23 +0200

    Update check_red5.sh

--- check_red5.sh
+++ check_red5.sh
@@ -2,17 +2,19 @@
 
 APP="http://example.com:8080"
 CURL="/usr/bin/curl"
-CHK=`${CURL} --silent -I ${APP}| head -1| awk {'print $2'}`
 COUNT=0
-PROBES=3
+PROBES=4
 
 while [ "${CHK}" != "200" ]; do
-    echo "Red5 continue starting..."
-    sleep 15
+    echo "Continue starting..."
+    sleep 30
     (( COUNT++ ))
+    CHK=`${CURL} --silent -I ${APP}| head -1| awk {'print $2'}`
 
     if [ ${COUNT} -ge ${PROBES} ]; then
-        echo "[ERROR]: Red5Pro failed to start after ($PROBES) probes."
+        echo "[ERROR]: Server failed to start after ($PROBES) probes."
         exit 1
     fi
 done
+
+echo "Server successfully started."
