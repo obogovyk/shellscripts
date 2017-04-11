9d8bd1b2e36402077ea1325df34a8bf5f69a5377
Oleksandr Bogovyk <obogovyk@gmail.com>
2017-04-11 12:18:28 +0300

    Update check_red5.sh

--- check_red5.sh
+++ check_red5.sh
@@ -3,7 +3,8 @@
 # Script: check_red5.sh
 # Author: Bogovyk Oleksandr <obogovyk@gmail.com>
 
-APP="http://red5.example.com"
+URL="${1:-example.com}"
+APP="http://$URL:5080"
 CURL="/usr/bin/curl"
 COUNT=0
 PROBES=4
@@ -12,11 +13,10 @@ while [ "${CHK}" != "200" ]; do
     echo "Red5 continue starting..."
     sleep 15
     (( COUNT+=1 ))
-    echo $COUNT
     CHK=$(${CURL} --silent -I ${APP}| head -1| awk {'print $2'})
 
     if [ ${COUNT} -ge ${PROBES} ]; then
-        echo "[ERROR]: Red5Pro failed to start after ($PROBES) probes."
+        echo "[ERROR]: Red5Pro failed to start after ${PROBES} probes."
         exit 1
     fi
 done
