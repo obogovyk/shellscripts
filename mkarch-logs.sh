97ca463ab6731fb67a8dffb5b64a5d74b11861c4
Oleksandr Bogovyk <obogovyk@gmail.com>
2017-04-11 12:24:02 +0300

    Update mkarch-logs.sh

--- mkarch-logs.sh
+++ mkarch-logs.sh
@@ -5,10 +5,10 @@ LATEST_N=$(ls | egrep -o '[0-9]*'|sort -n|tail -n 1)
 
 if [ ${#LOGS[@]} -gt 1 ]; then
     for i in ${LOGS[@]}; do
-        if [ $i != "runtime$LATEST_N.log" ]; then
-            tar cvzf "$i.tar.gz" $i
-            mv "$i.tar.gz" "../../logs-app/"
-            rm -f $i
+        if [ $i != "runtime${LATEST_N}.log" ]; then
+            tar cvzf "${i}.tar.gz" $i
+            mv "${i}.tar.gz" "../../logs-app/"
+            rm -f ${i}
         fi
     done
 else
