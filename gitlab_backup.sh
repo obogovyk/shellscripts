6cf78888688cc6999da5d1fe95f367b80ba1af43
Oleksandr Bogovyk <obogovyk@gmail.com>
2017-05-31 11:28:08 +0300

    Create gitlab_backup.sh

---
+++ gitlab_backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+export LC_ALL=en_US.utf8
+
+PREFIX="conf"
+GITLAB="gitlab"
+STORAGE="/mnt/storagebox"
+GITLAB_BACKDIR="${STORAGE}/${GITLAB}"
+GITLAB_WORKDIR="/var/opt/gitlab"
+DATE=$(date +%d.%m.%Y-%H.%M)
+TAR=$(which tar)
+RSYNC=$(which rsync)
+
+[ -z ${TAR} ] || [ -z ${RSYNC} ] &&  \
+{ echo "[INFO]: Necessary packages not found."; exit 1; }
+
+is_backdir_mounted(){
+    if [ $(cat /proc/mounts| grep "${STORAGE}"| wc -l) -lt 1 ]; then
+        echo "[ERROR]: Gitlab config backup aborted. ${GITLAB_BACKDIR} directory not mounted!" > /var/log/gitlab.mount.err.log
+    fi
+}
+
+backup_confdir() {
+    rm -rf ${GITLAB_BACKDIR}/*.tar.gz \
+    && cd /etc && cp -r ${GITLAB} "${PREFIX}-${GITLAB}-${DATE}" \
+    && ${TAR} -cvzf "${PREFIX}-${GITLAB}-${DATE}.tar.gz" "${PREFIX}-${GITLAB}-${DATE}" \
+    && mv "${PREFIX}-${GITLAB}-${DATE}.tar.gz" ${GITLAB_BACKDIR} \
+    && rm -rf "${PREFIX}-${GITLAB}-${DATE}"
+}
+
+is_backdir_mounted
+if [ $? -eq 0 ]; then
+    backup_confdir
+fi
+
+${RSYNC} -avz --modify-window=1 ${GITLAB_WORKDIR}/backups/ ${GITLAB_BACKDIR}/backups/
