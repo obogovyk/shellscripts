a7302bef9a0ccdd991fb5f776f262f62c1c765bd
Oleksandr Bogovyk <obogovyk@gmail.com>
2017-07-28 19:46:27 +0300

    Create new file

---
+++ docker_export.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export LC_ALL=en_US.utf8
+
+DOCKER=$(which docker)
+STORAGE="/mnt/storagebox"
+BKP_DIR="${STORAGE}/docker"
+DATE_FMT=$(date +%d%m%Y)
+CONTAINER_ID="0a77b61f5229"
+VALUE=$(cat docker_id)
+PROJECT="melochenator"
+
+[ -z ${DOCKER} ] && { echo "[INFO]: Required packages (docker) not found."; exit 1; }
+
+increment_id() {
+    echo "$(( ${VALUE}+1 ))" > docker_id
+}
+
+docker_export() {
+    ${DOCKER} export ${CONTAINER_ID} > ${BKP_DIR}/${PROJECT}_${DATE_FMT}_${VALUE}.tar
+}
+
+is_backdir_mounted(){
+    if [ $(cat /proc/mounts| grep "${STORAGE}"| wc -l) -lt 1 ]; then
+        echo "[ERROR]: Docker backup aborted. ${STORAGE} directory not mounted!" > /var/log/docker.export.err.log
+    fi
+}
+
+is_backdir_mounted
+if [ $? -eq 0 ]; then
+    docker_export
+    increment_id
+fi
