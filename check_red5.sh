5771ace9146f0a1ef3c9ba64a7e638f2a7349f39
Oleksandr Bogovyk <obogovyk@gmail.com>
2017-08-07 19:14:21 +0300

    Update check_red5.sh

--- check_red5.sh
+++ check_red5.sh
@@ -3,18 +3,20 @@
 # Script: check_red5.sh
 # Author: Bogovyk Oleksandr <obogovyk@gmail.com>
 
-URL="${1:-example.com}"
-PORT=5080
-APP="http://${URL}:${PORT}"
-CURL=$(which curl)
+#!/bin/bash
+
+HOST="${1:-red5.inti.live}"
+PORT=80
+URL="http://${HOST}:${PORT}"
+CURL="/usr/bin/curl"
 COUNT=0
 PROBES=4
 
 while [ "${CHK}" != "200" ]; do
-    echo "Red5 continue starting..."
+    echo "Red5 starting..."
     sleep 15
-    (( COUNT+=1 ))
-    CHK=$(${CURL} --silent -I ${APP}| head -1| awk {'print $2'})
+    (( COUNT++ ))
+    CHK=$(${CURL} --silent -I ${URL}| head -1| awk {'print $2'})
 
     if [ ${COUNT} -ge ${PROBES} ]; then
         echo "[ERROR]: Red5Pro failed to start after ${PROBES} probes."
