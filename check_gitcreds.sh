b5590b5a3f732bfdbc9cae1d38b08d7066c6c2fc
Oleksandr Bogovyk <obogovyk@gmail.com>
2017-08-17 12:57:46 +0300

    Update check_gitcreds.sh

--- check_gitcreds.sh
+++ check_gitcreds.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-USER="$1"
+# Description: Find usernames in .git/config file
+
+USER=$1
 DIRS=`ls`
 for i in ${DIRS[*]}; do
     if [ -d $i ]; then
-        grep "$USER:" $i/.git/config && echo $i
+        grep "${USER}:" ${i}/.git/config && echo ${i}
     fi
 done
