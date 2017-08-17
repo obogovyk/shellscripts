5a8c64b2a640a370e4a200a0466429e6d0ba0799
Oleksandr Bogovyk <obogovyk@gmail.com>
2017-08-17 12:50:34 +0300

    Update hdd_sched_test.sh

--- hdd_sched_test.sh
+++ hdd_sched_test.sh
@@ -3,14 +3,14 @@
 # Author: Oleksandr Bogovyk
 # Script: HDD Scheduler test
 
-DISC="sda"
+DISC=$1
 
 echo "Default scheduler: "
 cat /sys/block/$DISC/queue/scheduler
 echo ""
 
 for S in $(sed "s/\[//;s/\]//" /sys/block/sda/queue/scheduler); do 
-    echo $S > /sys/block/$DISC/queue/scheduler;
+    echo $S > /sys/block/${DISC}/queue/scheduler;
 
     cat /sys/block/$DISC/queue/scheduler
     sync && /sbin/hdparm -tT /dev/$DISC && echo "----"
