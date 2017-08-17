d5ba7591450d40163568ab61f94b52b9f82cf049
Oleksandr Bogovyk <obogovyk@gmail.com>
2017-08-17 12:53:17 +0300

    Update conf_backup.sh

--- conf_backup.sh
+++ conf_backup.sh
@@ -3,13 +3,14 @@
 export LC_ALL=en_US.utf8
 
 TAR=$(which tar)
-STORAGE="/mnt/storage"
+STORAGE="/path/to/dir"
 BKP_DIR="${STORAGE}/conf.dev"
 DATE=$(date +%d.%m.%Y-%H.%M)
 
 [ -z ${TAR} ] && { echo "[INFO]: Required packages (tar) not found."; exit 1; }
 
 FILES=(
+    /etc/letsencrypt/archive
     /etc/phpMyAdmin
     /etc/sssd
     /etc/zabbix
@@ -18,9 +19,14 @@ FILES=(
     /etc/sysctl.conf
     /etc/php-fpm.conf
     /etc/php.ini
+    /etc/nginx/.htpasswd
     /etc/nginx/conf.d
+    /etc/nginx/nginx.conf
     /etc/security/limits.conf
-    /home/{user}/.ssh
+    /etc/sysconfig/iptables
+    /home/centos/.ssh
+    /home/deploy/.ssh
+    /opt/scripts
 )
 
 is_backdir_mounted(){
@@ -34,4 +40,4 @@ if [ $? -eq 0 ]; then
     cd ${BKP_DIR} && ${TAR} -cvzf conf.dev-${DATE}.tar.gz -P ${FILES[@]}
 fi
 
-find ${BKP_DIR} -name "conf*" -type d -mtime +7 -delete
+find ${BKP_DIR} -name "conf*" -type f -mtime +6 -delete
