1ca7b1edd83eddf2c189d8ef801196e0728aa114
Oleksandr Bogovyk <obogovyk@gmail.com>
2017-12-28 17:19:06 +0200

    Create signin_loop.sh

---
+++ signin_loop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CERT_PATH="${WORKSPACE}/fastlane/signing/certificates"
+encrypt() {
+    CERTS=($(ls))
+
+    for i in ${CERTS[@]}; do 
+        /usr/bin/openssl aes-256-cbc -k {{ password }} -in ${i} -salt -d -a -out "${i%.p12.enc}.p12"
+    done
+}
+
+cd ${CERT_PATH} && encrypt
