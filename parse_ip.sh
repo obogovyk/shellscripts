0707cc32f521570246e1f4711625fee474027196
Oleksandr Bogovyk <obogovyk@gmail.com>
2018-01-24 13:24:24 +0200

    Update parse_ip.sh

--- parse_ip.sh
+++ parse_ip.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Script: parse_ip.sh
-# Description: parse NGINX log file for uniq IPs more that ${LIMIT} variable
+# Description: parse NGINX log file for uniq IPs more that ${LIMIT} conn. variable
 
 LOGFILE=$1
 UNIQ_IPS=`cat ${LOGFILE}| awk {'print $1'}| grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"| sort| uniq`
-LIMIT=30
+LIMIT=25
 
 for i in ${UNIQ_IPS[@]}; do
     if [ "$(grep -c ${i} ${LOGFILE})" -ge "${LIMIT}" ]; then
