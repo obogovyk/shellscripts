575e066eac00825dc3c74321bb9ba18e37951e73
Oleksandr Bohovyk <obohovyk@posteo.net>
2018-03-13 17:33:00 +0200

    Update hdd_sched_test.sh

--- hdd_sched_test.sh
+++ hdd_sched_test.sh
@@ -13,5 +13,7 @@ for S in $(sed "s/\[//;s/\]//" /sys/block/sda/queue/scheduler); do
     echo $S > /sys/block/${DISC}/queue/scheduler;
 
     cat /sys/block/$DISC/queue/scheduler
-    sync && /sbin/hdparm -tT /dev/$DISC && echo "----"
+    sync \
+    && /sbin/hdparm -tT /dev/$DISC \
+    && echo "----"
     sleep 5
