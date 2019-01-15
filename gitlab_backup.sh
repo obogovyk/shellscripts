e90c5400986506ad9c3ad6f6be46ba2354cbd434
Oleksandr <obohovyk@posteo.net>
2019-01-15 18:02:39 +0200

    Update gitlab_backup.sh

--- gitlab_backup.sh
+++ gitlab_backup.sh
@@ -2,10 +2,11 @@
 
 export LC_ALL=en_US.utf8
 
-PREFIX="conf"
+PREFIX="config"
 GITLAB="gitlab"
-STORAGE="/path/to/storage"
-GITLAB_BACKDIR="${STORAGE}/${GITLAB}"
+STORAGE="/mnt2/storagebox"
+GITLAB_DUMPDIR="${STORAGE}/${GITLAB}.db"
+GITLAB_CONFDIR="${STORAGE}/${GITLAB}.${PREFIX}"
 GITLAB_WORKDIR="/var/opt/gitlab"
 DATE=$(date +%d.%m.%Y-%H.%M)
 TAR=$(which tar)
@@ -13,7 +14,7 @@ RSYNC=$(which rsync)
 
 [ -z ${TAR} ] || [ -z ${RSYNC} ] && { echo "[INFO]: Required packages not found."; exit 1; }
 
-is_backdir_mounted(){
+is_backdir_mounted() {
     if [ $(cat /proc/mounts| grep "${STORAGE}"| wc -l) -lt 1 ]; then
         echo "[ERROR]: Gitlab config backup aborted. ${STORAGE} directory not mounted!" > /var/log/gitlab.backup.err.log
     fi
@@ -22,7 +23,7 @@ is_backdir_mounted(){
 backup_confdir() {
     cd /etc && cp -r ${GITLAB} "${PREFIX}-${GITLAB}-${DATE}" \
     && ${TAR} -cvzf "${PREFIX}-${GITLAB}-${DATE}.tar.gz" "${PREFIX}-${GITLAB}-${DATE}" \
-    && mv "${PREFIX}-${GITLAB}-${DATE}.tar.gz" ${GITLAB_BACKDIR} \
+    && mv "${PREFIX}-${GITLAB}-${DATE}.tar.gz" "${GITLAB_CONFDIR}" \
     && rm -rf "${PREFIX}-${GITLAB}-${DATE}"
 }
 
@@ -31,5 +32,6 @@ if [ $? -eq 0 ]; then
     backup_confdir
 fi
 
-find ${GITLAB_BACKDIR}/ -name "conf*" -type f -mtime +6 -delete
-${RSYNC} -avz --modify-window=1 ${GITLAB_WORKDIR}/backups/ ${GITLAB_BACKDIR}/backups/ --delete
+find ${GITLAB_CONFDIR} -name "conf*" -type f -mtime +6 -delete
+
+${RSYNC} -avz --modify-window=1 ${GITLAB_WORKDIR}/backups/ ${GITLAB_DUMPDIR} --delete
