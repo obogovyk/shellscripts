b659b5a0641bb38ce56041459cbdd791c31c787a
Oleksandr <obohovyk@posteo.net>
2019-01-15 18:05:12 +0200

    Create certbot_autoupdate.sh

---
+++ certbot_autoupdate.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+export LC_LANG=en_US.utf8
+
+CERT_LIST=$(ls /etc/letsencrypt/live)
+CERT_DISABLE=("p12.dev.example.com" "info.stage.example.com")
+SORTED=()
+
+for x in ${CERT_LIST[@]}; do
+    skip=
+    for y in ${CERT_DISABLE[@]}; do
+        [[ $x == $y ]] && { skip=1; break; }
+    done
+    [[ -n $skip ]] || SORTED+=("${x}")
+done
+
+for i in ${SORTED[@]}; do
+    CERT=/etc/letsencrypt/live/${i}/cert.pem
+
+    EXPD=`openssl x509 -enddate -noout -in ${CERT} | awk -F'=' '{print $2}'`
+    CERTD="date +%D --date='${EXPD}'"
+    ED=`eval ${CERTD}`
+    TODAYD=$(date -d "+7 days")
+
+    if [ "`date -d "${ED}" +%s`" -le "`date -d "${TODAYD}" +%s`" ]; then
+        echo "Certificate: ${i} will be updated."
+        certbot certonly --force-renew --webroot --webroot-path=/usr/share/nginx/html/${i} --cert-name ${i}
+        echo "DONE."
+    fi
+done
