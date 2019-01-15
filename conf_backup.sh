c5b86f23e35f2703aa09795ef885441a63196cc2
Oleksandr <obohovyk@posteo.net>
2019-01-15 18:07:49 +0200

    Update conf_backup.sh

--- conf_backup.sh
+++ conf_backup.sh
@@ -1,43 +1,50 @@
 #!/bin/bash
 
-export LC_ALL=en_US.utf8
+export LC_LANG=en_US.utf8
 
 TAR=$(which tar)
-STORAGE="/path/to/dir"
-BKP_DIR="${STORAGE}/conf.dev"
+STORAGE="/mnt/storagebox"
+BKP_DIR="${STORAGE}/dev.config"
 DATE=$(date +%d.%m.%Y-%H.%M)
 
 [ -z ${TAR} ] && { echo "[INFO]: Required packages (tar) not found."; exit 1; }
 
 FILES=(
     /etc/letsencrypt/archive
+    /etc/.storagebox.txt
     /etc/phpMyAdmin
     /etc/sssd
     /etc/zabbix
     /etc/my.cnf
     /etc/mongod.conf
     /etc/sysctl.conf
+    /etc/rsyslog.conf
     /etc/php-fpm.conf
     /etc/php.ini
-    /etc/nginx/.htpasswd
     /etc/nginx/conf.d
+    /etc/nginx/nginx-ldap-auth
     /etc/nginx/nginx.conf
+    /etc/nginx/proxy_params
+    /etc/nginx/proxy_buffering_params
+    /etc/nginx/websocket_params
+    /etc/nginx/.htpasswd
     /etc/security/limits.conf
     /etc/sysconfig/iptables
     /home/centos/.ssh
     /home/deploy/.ssh
+    /home/deploy/.pm2/dump.pm2
     /opt/scripts
 )
 
 is_backdir_mounted(){
-    if [ $(cat /proc/mounts| grep "${STORAGE}"| wc -l) -lt 1 ]; then
+    if [ $(cat /proc/mounts| grep -c "${STORAGE}") -lt 1 ]; then
         echo "[ERROR]: Config backup aborted. ${STORAGE} directory not mounted!" > /var/log/conf.backup.err.log
     fi
 }
 
 is_backdir_mounted
 if [ $? -eq 0 ]; then
-    cd ${BKP_DIR} && ${TAR} -cvzf conf.dev-${DATE}.tar.gz -P ${FILES[@]}
+    cd ${BKP_DIR} && ${TAR} -cvzf dev.config-${DATE}.tar.gz -P ${FILES[@]}
 fi
 
-find ${BKP_DIR} -name "conf*" -type f -mtime +6 -delete
+find ${BKP_DIR} -name "dev.conf*" -type f -mtime +6 -delete
