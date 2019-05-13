b8fe862545c6b9a3e66ddca903b751e9b3333609
Oleksandr <obohovyk@posteo.net>
2019-05-13 12:58:43 +0000

    Update certbot_autoupdate.sh

--- certbot_autoupdate.sh
+++ certbot_autoupdate.sh
@@ -3,7 +3,7 @@
 export LC_LANG=en_US.utf8
 
 CERT_LIST=$(ls /etc/letsencrypt/live)
-CERT_DISABLE=("p12.dev.example.com" "info.stage.example.com")
+CERT_DISABLE=("")
 SORTED=()
 
 for x in ${CERT_LIST[@]}; do
@@ -20,11 +20,12 @@ for i in ${SORTED[@]}; do
     EXPD=`openssl x509 -enddate -noout -in ${CERT} | awk -F'=' '{print $2}'`
     CERTD="date +%D --date='${EXPD}'"
     ED=`eval ${CERTD}`
-    TODAYD=$(date -d "+7 days")
+    TODAYD=$(date -d "+14 days")
 
     if [ "`date -d "${ED}" +%s`" -le "`date -d "${TODAYD}" +%s`" ]; then
         echo "Certificate: ${i} will be updated."
-        certbot certonly --force-renew --webroot --webroot-path=/usr/share/nginx/html/${i} --cert-name ${i}
-        echo "Certificate ${i} has been updated."
+        certbot certonly --dns-google --dns-google-credentials /etc/letsencrypt/.secrets/google.json --dns-google-propagation-seconds 90 --force-renewal -d ${i}
+#        certbot certonly --force-renew --webroot --webroot-path=/var/www/${i} --cert-name ${i}
+        echo "Certificate ${i} has been successfully updated."
     fi
 done
