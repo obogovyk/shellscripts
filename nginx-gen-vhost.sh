983f28ff1971d963b8117fb8c441ad43308686bc
Oleksandr <obohovyk@posteo.net>
2019-08-05 16:17:06 +0300

    Create nginx-gen-vhost.sh

---
+++ nginx-gen-vhost.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# domain list must be specified via $1 parameter variable
+
+VHOST="vhost.conf.txt"
+
+usage() {
+    echo "Usage: $0 <Specify NGINX femplate file>"
+}
+
+if [ $# -ne 1 ]
+then
+    usage
+    exit 1
+fi
+
+while DOMAIN= read -r dom; do
+    sed "s/{DOMAIN}/$dom/g" $VHOST > ${dom}.conf
+done < "$1"
