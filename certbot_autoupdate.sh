f1ee046d8070bbdf940a3af24e13ab5ca5d84221
Oleksandr <obohovyk@posteo.net>
2019-10-02 14:50:34 +0300

    Update certbot_autoupdate.sh

--- certbot_autoupdate.sh
+++ certbot_autoupdate.sh
@@ -5,6 +5,7 @@ export LC_LANG=en_US.utf8
 CERT_LIST=$(ls /etc/letsencrypt/live)
 CERT_DISABLE=("")
 SORTED=()
+DBU=15
 
 for x in ${CERT_LIST[@]}; do
     skip=
@@ -20,11 +21,11 @@ for i in ${SORTED[@]}; do
     EXPD=`openssl x509 -enddate -noout -in ${CERT} | awk -F'=' '{print $2}'`
     CERTD="date +%D --date='${EXPD}'"
     ED=`eval ${CERTD}`
-    TODAYD=$(date -d "+12 days")
+    TODAYD=$(date -d "+${DBU} days")
 
     if [ "`date -d "${ED}" +%s`" -le "`date -d "${TODAYD}" +%s`" ]; then
         echo "Certificate: ${i} will be updated."
-        certbot certonly --dns-google --dns-google-credentials /etc/letsencrypt/.secrets/google.json --dns-google-propagation-seconds 60 --force-renewal -d ${i}
+        certbot certonly --dns-google --dns-google-credentials /etc/letsencrypt/.secrets/google.json --dns-google-propagation-seconds 90 --force-renewal -d ${i}
         service nginx reload
     fi
 done
